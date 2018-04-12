80db4d126e7c3522d0819a6acac26d553eeec07d
Vadim <legostaev.vadim@mail.ru>
2018-04-12 23:31:17 +0300

    edit

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const pretty = require('pretty'),
+const beautify = require('js-beautify').html,
     through2 = require('through2').obj;
 
 module.exports = function(options = {}) {
     return through2(function(file, enc, callback) {
-        file.contents = Buffer.from(pretty(file.contents.toString(), {
-            unformatted: options.unformatted || ['code', 'pre', 'em', 'strong', 'br'],
-            indent_size: options.indent || 4
+        file.contents = Buffer.from(beautify(file.contents.toString(), {
+            unformatted: options.unformatted || ['code', 'pre', 'em', 'strong', 'i', 'b', 'br', 'span'],
+            indent_char: options.indent_char || ' ',
+            indent_size: options.indent_size || 4
         }));
         callback(null, file);
     });
