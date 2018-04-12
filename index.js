1701162d292dad06e726368406ff55687eb45402
Vadim <legostaev.vadim@mail.ru>
2018-04-13 02:52:38 +0300

    edit

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const beautify = require('js-beautify').html,
+const beautify = require('js-beautify'),
     through2 = require('through2').obj;
 
+
 module.exports = function(options = {}) {
+
+    options.indent_size = options.indent_size || 4;
+    options.indent_char = options.indent_char || ' ';
+    options.unformatted = options.unformatted || ['code', 'pre', 'em', 'strong', 'i', 'b', 'br', 'span'];
+
     return through2(function(file, enc, callback) {
-        file.contents = Buffer.from(beautify(file.contents.toString(), {
-            unformatted: options.unformatted || ['code', 'pre', 'em', 'strong', 'i', 'b', 'br', 'span'],
-            indent_char: options.indent_char || ' ',
-            indent_size: options.indent_size || 4
-        }));
+
+        if (file.isNull()) return callback(null, file);
+        if (file.isStream()) return callback(new Error('gulp-pretty-html: Streaming not supported'));
+
+        file.contents = Buffer.from(beautify.html(file.contents.toString(), options));
+
         callback(null, file);
+
     });
 }
